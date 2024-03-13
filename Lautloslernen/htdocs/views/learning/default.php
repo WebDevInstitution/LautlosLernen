469e0e64685df45a9b14a479c2f25b56bb3b6cc8
sylvan013 <kai.keppler@web.de>
2024-03-13 14:30:21 +0100

    rework: structure code after mvc model

--- Lautloslernen/htdocs/views/learning/default.php
+++ Lautloslernen/htdocs/views/learning/default.php
@@ -1,70 +1,7 @@
-
-<head>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-</head>
-
-<?php
-include __DIR__ . "/../../config.php";
-include __DIR__ . "/../../Framework/dashboardRepository.php";
-
-class defaultLearning {
-    private $dashboardRepo;
-    private $date;
-    private $letters;
-    private $letterToGuess;
-
-    public function __construct() {
-        global $config;
-        global $letters;
-        $this->dashboardRepo = new dashboardRepository($config);
-        $this->date = $this->getDate();
-        $this->letters = $letters;
-    }
-
-    public function checkAnswerAndGetNewLetter() {
-        $this->checkAnswer();
-        $this->getLetterToGuess();
-    }
-
-    private function getLetterToGuess() {
-        $this->letterToGuess = $this->letters[random_int(0, count($this->letters) - 1)];
-    }
-
-    private function getDate() {
-        return date("Y-m-d");
-    }
-    
-    public function checkAnswer() {
-        if (isset($_POST["userGuess"])) {
-            $userGuess = $_POST["userGuess"];
-            if ($userGuess == $this->letterToGuess){
-                $this->dashboardRepo->incrementCorrectAnswer("", $this->letterToGuess, $this->date);
-            } else {
-                $this->dashboardRepo->incrementWrongAnswer("", $this->letterToGuess, $this->date);
-            }
-        }
-    }
-}
-
-$deafultLearning = new defaultLearning();
-
-if (isset($_POST["action"]) && $_POST["action"] === "callCheckAnswerAndGetNewLetter") {
-    $deafultLearning->checkAnswerAndGetNewLetter();
-}
-
-if (isset($_POST["action"]) && $_POST["action"] === "callGetLetterToGuess") {
-    $deafultLearning->checkAnswerAndGetNewLetter();
-}
-
-?>
-
-<button type="submit" onclick="callCheckAnswerAndGetNewLetter()">Antowort best&auml;igen</button>
-<button type="submit" onclick="callGetLetterToGuess()">Neuer Buchstabe</button>
-
 <script>
     function callCheckAnswerAndGetNewLetter() {
             var xhr = new XMLHttpRequest();
-            xhr.open("POST", "http://localhost:8080/views/learning/default.php", true);
+            xhr.open("POST", "http://localhost:8080/controllers/learningController.php", true);
             xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
             xhr.onreadystatechange = function () {
                 if (xhr.readyState === 4 && xhr.status === 200) {
@@ -75,7 +12,7 @@ if (isset($_POST["action"]) && $_POST["action"] === "callGetLetterToGuess") {
 
         function callGetLetterToGuess() {
             var xhr = new XMLHttpRequest();
-            xhr.open("POST", "http://localhost:8080/views/learning/default.php", true);
+            xhr.open("POST", "http://localhost:8080/controllers/learningController.php", true);
             xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
             xhr.onreadystatechange = function () {
                 if (xhr.readyState === 4 && xhr.status === 200) {
@@ -87,6 +24,11 @@ if (isset($_POST["action"]) && $_POST["action"] === "callGetLetterToGuess") {
 
 <div id="webcam-container"></div>
 <div id="label-container"></div>
+
+<button type="submit" onclick="callCheckAnswerAndGetNewLetter()">Antowort best&auml;igen</button>
+<button type="submit" onclick="callGetLetterToGuess()">Neuer Buchstabe</button>
+
 <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest/dist/tf.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@teachablemachine/image@latest/dist/teachablemachine-image.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script type="text/javascript" src="http://localhost:8080/views/learning/teachableMachine.js"></script>
