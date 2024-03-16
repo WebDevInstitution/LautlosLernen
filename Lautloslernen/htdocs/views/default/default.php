a0fb218e333b5b637edcf6ab5edf64e7dc4cd67e
sylvan013 <kai.keppler@web.de>
2024-03-16 13:09:20 +0100

    feat: changed style of website

--- Lautloslernen/htdocs/views/default/default.php
+++ Lautloslernen/htdocs/views/default/default.php
@@ -1,7 +1,6 @@
-
+<main>
     <h1>Entdecke die faszinierende Welt der Gebärdensprache</h1>
 
-<main>
     <p>Liebe Besucherinnen und Besucher,</p>
     <p>Wir heißen Sie herzlich auf unserer Webseite willkommen! Sind Sie neugierig darauf, eine neue Sprache zu lernen und gleichzeitig die Welt der Gehörlosen und ihrer einzigartigen Kultur zu entdecken? Dann sind Sie hier genau richtig!</p>
     <p>Auf unserer Webseite bieten wir eine einzigartige Möglichkeit, die Gebärdensprache auf eine interaktive und effektive Weise zu erlernen. Dank unserer KI-gestützten Plattform können Sie die Grundlagen und Feinheiten dieser faszinierenden Sprache in Ihrem eigenen Tempo und nach Ihren individuellen Bedürfnissen erlernen.</p>
@@ -9,9 +8,6 @@
     <p>Unser Team von Experten steht Ihnen dabei zur Seite, um Ihnen ein inspirierendes und unterstützendes Lernumfeld zu bieten. Unser Ziel ist es, Ihnen nicht nur die Sprache selbst, sondern auch ihre kulturelle Bedeutung und ihre Rolle in der Gesellschaft näherzubringen.</p>
     <p>Also, worauf warten Sie noch? Tauchen Sie ein in die faszinierende Welt der Gebärdensprache und beginnen Sie noch heute Ihre Reise mit uns! Besuchen Sie unsere Webseite und entdecken Sie die Schönheit und Vielfalt dieser einzigartigen Sprache.</p>
     <p>Wir freuen uns darauf, Sie auf Ihrer Reise zu begleiten!</p>
+
+    <iframe width="800" height="450" src="https://www.youtube.com/embed/Cn2Nle9Vca0?si=h7r2leCFcGF3UYRp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 </main>
-
-<iframe width="800" height="450" src="https://www.youtube.com/embed/Cn2Nle9Vca0?si=h7r2leCFcGF3UYRp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    <br>
-
-<a href="/?a=registration&c=user" class="nav-link" style="background-color: grey; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; font-size: 16px; ; cursor: pointer;">Registrieren</a>
