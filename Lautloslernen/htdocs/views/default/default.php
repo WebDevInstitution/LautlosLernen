f4c2f827ec1284d4f6ede18a2a66e46c41a8c0a2
sylvan013 <kai.keppler@web.de>
2024-03-19 15:19:17 +0100

    feat: add new homescreen

--- Lautloslernen/htdocs/views/default/default.php
+++ Lautloslernen/htdocs/views/default/default.php
@@ -1,16 +1,43 @@
 <main>
-    <h1>Entdecke die faszinierende Welt der Gebärdensprache</h1>
-
-    <h2>Warum Gebärdensprache lernen?</h2>
-    <p>Wir heißen Dich herzlich auf unserer Webseite willkommen! Bist Du neugierig darauf, eine neue Sprache zu lernen und gleichzeitig die Welt der Gehörlosen und Tauben und ihrer einzigartigen Kultur zu entdecken? Das kann sich lohnen:</p>
+    <h1>LautlosLernen - Entdecke die faszinierende Welt der Gebärdensprache</h1>
+    <p>Herzlich willkommen auf unserer Website!</p>
+    <p>
+        Du willst wissen, wer wir sind und was wir machen? Dann schau doch mal hier nach:
+        <nav>
+            <ul>
+                <li><a href="#warum-gebaerdensprache-lernen">Warum Gebärdensprache lernen?</a></li>
+                <li><a href="#unsere-vision">Unsere Vision</a></li>
+                <li><a href="#unsere-technologie">Unsere Technologie</a></li>
+            </ul>
+        </nav>
+    </p>
+    
+    <h2 id="warum-gebaerdensprache-lernen">Warum Gebärdensprache lernen?</h2>
+    <p>
+        Du hast jemanden kennengelernt mit dem Du nicht kommunizieren kannst wie gewohnt?
+        <br>Deine Lebensumstände haben sich geändert und auf einmal reichen Worte nicht mehr aus?
+        <br>Oder bist Du einfach nur neugierig darauf, eine neue Sprache zu lernen, obwohl Du sie schon sprichst?
+        <br>Egal aus welchen Grund, es kann sich lohnen, Gebärdensprache zu lernen:
+    </p>
     <img src="../img/statistik.png" alt="Statistik zur Anzahl der Schwerhörigen und Menschen mit Sprach- oder Sprechstörungen in Deutschland nach Art der Behinderung in den Jahren 2017 bis 2021" width=80% height=60%>
-    <h2>Unsere Technologie</h2>
-    <p>Auf unserer Webseite bieten wir eine einzigartige Möglichkeit, die Gebärdensprache auf eine interaktive und effektive Weise zu erlernen. Dank unserer KI-gestützten Plattform kannst Du die Grundlagen und Feinheiten dieser faszinierenden Sprache in Deinem eigenen Tempo und nach Deinen individuellen Bedürfnissen erlernen.</p>
-    <p>Die Gebärdensprache ist viel mehr als nur eine Kommunikationsform. Sie ist eine Brücke, die uns verbindet und uns ermöglicht, die Welt aus einer neuen Perspektive zu betrachten. Indem Du Gebärdensprache lernst, öffnen sich Türen zu neuen Freundschaften, beruflichen Chancen und einem tieferen Verständnis für die Vielfalt unserer Gesellschaft.</p>
-    <h2>Unsere Vision</h2>
-    <p>Unser Team von Experten steht Dir dabei zur Seite, um Dir ein inspirierendes und unterstützendes Lernumfeld zu bieten. Unser Ziel ist es nicht nur, Dir eine neue Möglichkeit der Kommunikation beizubringen, unsere Vision.</p>
-    <p>Also, worauf wartest Du noch? Tauche ein, in die faszinierende Welt der Gebärdensprache und beginne noch heute Deine Reise mit uns!</p>
+    <p>Indem Du Gebärdensprache lernst, öffnen sich Türen zu neuen Freundschaften, beruflichen Chancen und einem tieferen Verständnis für die Vielfalt unserer Gesellschaft.</p>
+    
+    <h2 id="unsere-vision">Unsere Vision</h2>
+    <p>
+        Die Gebärdensprache ist viel mehr als nur eine Kommunikationsform. Sie ist eine Brücke, die uns mit denen verbindet, die auf konventionelle Art nicht an unserem Miteinander teilhaben können. 
+        <br>Sie ermöglicht uns, die Welt aus einer neuen Perspektive zu betrachten und von Menschen zu lernen, die das Leben auf eine andere Art erfahren.
+    </p>
+    <p>
+        Unser Team von Experten steht Dir dabei zur Seite, um Dir ein inspirierendes und unterstützendes Lernumfeld zu bieten. 
+        <br>Unser Ziel ist es nicht nur, eine neue Möglichkeit der Kommunikation beizubringen, unsere Vision ist die Inklussion aller Menschen in unsere Gesellschaft.
+        <br>Diese Plattform soll unser Beitrag zu dieser Vision leisten.
+    </p>
     <p>Wir freuen uns darauf, Dich auf Deiner Reise zu begleiten!</p>
-
+    
+    <h2 id="unsere-technologie">Unsere Technologie</h2>
+    <p>
+        Auf unserer Webseite bieten wir eine einzigartige Möglichkeit, die Gebärdensprache auf eine interaktive und effektive Weise zu erlernen.
+        <br>Dank unserer KI-gestützten Plattform kannst Du die Grundlagen und Feinheiten dieser faszinierenden Sprache in Deinem eigenen Tempo und nach Deinen individuellen Bedürfnissen erlernen.
+    </p>
     <iframe width="800" height="450" src="https://www.youtube.com/embed/Cn2Nle9Vca0?si=h7r2leCFcGF3UYRp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 </main>
