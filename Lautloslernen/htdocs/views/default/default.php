feba4bef57f63b8a1d98f19399e114a2bbf61b4a
sylvan013 <kai.keppler@web.de>
2024-03-19 14:48:21 +0100

    feat: add new styles and format homepage

--- Lautloslernen/htdocs/views/default/default.php
+++ Lautloslernen/htdocs/views/default/default.php
@@ -1,13 +1,16 @@
 <main>
     <h1>Entdecke die faszinierende Welt der Gebärdensprache</h1>
 
-    <p>Liebe Besucherinnen und Besucher,</p>
-    <p>Wir heißen Sie herzlich auf unserer Webseite willkommen! Sind Sie neugierig darauf, eine neue Sprache zu lernen und gleichzeitig die Welt der Gehörlosen und ihrer einzigartigen Kultur zu entdecken? Dann sind Sie hier genau richtig!</p>
-    <p>Auf unserer Webseite bieten wir eine einzigartige Möglichkeit, die Gebärdensprache auf eine interaktive und effektive Weise zu erlernen. Dank unserer KI-gestützten Plattform können Sie die Grundlagen und Feinheiten dieser faszinierenden Sprache in Ihrem eigenen Tempo und nach Ihren individuellen Bedürfnissen erlernen.</p>
-    <p>Die Gebärdensprache ist viel mehr als nur eine Kommunikationsform. Sie ist eine Brücke, die uns verbindet und uns ermöglicht, die Welt aus einer neuen Perspektive zu betrachten. Indem Sie Gebärdensprache lernen, öffnen sich Türen zu neuen Freundschaften, beruflichen Chancen und einem tieferen Verständnis für die Vielfalt unserer Gesellschaft.</p>
-    <p>Unser Team von Experten steht Ihnen dabei zur Seite, um Ihnen ein inspirierendes und unterstützendes Lernumfeld zu bieten. Unser Ziel ist es, Ihnen nicht nur die Sprache selbst, sondern auch ihre kulturelle Bedeutung und ihre Rolle in der Gesellschaft näherzubringen.</p>
-    <p>Also, worauf warten Sie noch? Tauchen Sie ein in die faszinierende Welt der Gebärdensprache und beginnen Sie noch heute Ihre Reise mit uns! Besuchen Sie unsere Webseite und entdecken Sie die Schönheit und Vielfalt dieser einzigartigen Sprache.</p>
-    <p>Wir freuen uns darauf, Sie auf Ihrer Reise zu begleiten!</p>
+    <h2>Warum Gebärdensprache lernen?</h2>
+    <p>Wir heißen Dich herzlich auf unserer Webseite willkommen! Bist Du neugierig darauf, eine neue Sprache zu lernen und gleichzeitig die Welt der Gehörlosen und Tauben und ihrer einzigartigen Kultur zu entdecken? Das kann sich lohnen:</p>
+    <img src="../img/statistik.png" alt="Statistik zur Anzahl der Schwerhörigen und Menschen mit Sprach- oder Sprechstörungen in Deutschland nach Art der Behinderung in den Jahren 2017 bis 2021" width=80% height=60%>
+    <h2>Unsere Technologie</h2>
+    <p>Auf unserer Webseite bieten wir eine einzigartige Möglichkeit, die Gebärdensprache auf eine interaktive und effektive Weise zu erlernen. Dank unserer KI-gestützten Plattform kannst Du die Grundlagen und Feinheiten dieser faszinierenden Sprache in Deinem eigenen Tempo und nach Deinen individuellen Bedürfnissen erlernen.</p>
+    <p>Die Gebärdensprache ist viel mehr als nur eine Kommunikationsform. Sie ist eine Brücke, die uns verbindet und uns ermöglicht, die Welt aus einer neuen Perspektive zu betrachten. Indem Du Gebärdensprache lernst, öffnen sich Türen zu neuen Freundschaften, beruflichen Chancen und einem tieferen Verständnis für die Vielfalt unserer Gesellschaft.</p>
+    <h2>Unsere Vision</h2>
+    <p>Unser Team von Experten steht Dir dabei zur Seite, um Dir ein inspirierendes und unterstützendes Lernumfeld zu bieten. Unser Ziel ist es nicht nur, Dir eine neue Möglichkeit der Kommunikation beizubringen, unsere Vision.</p>
+    <p>Also, worauf wartest Du noch? Tauche ein, in die faszinierende Welt der Gebärdensprache und beginne noch heute Deine Reise mit uns!</p>
+    <p>Wir freuen uns darauf, Dich auf Deiner Reise zu begleiten!</p>
 
     <iframe width="800" height="450" src="https://www.youtube.com/embed/Cn2Nle9Vca0?si=h7r2leCFcGF3UYRp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 </main>
