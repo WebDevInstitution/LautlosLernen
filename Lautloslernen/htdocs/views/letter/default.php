1e3fd7054148858aafa053e481bf6c5e230787bb
Timbuc123 <tim.buckenthien@gmail.com>
2024-03-19 21:35:09 +0100

    Gebärden Alphabet
    Ergänzung des Gebärden Alphabets

--- Lautloslernen/htdocs/views/letter/default.php
+++ Lautloslernen/htdocs/views/letter/default.php
@@ -1,12 +1,10 @@
-Hier wollen wir die Buchstaben sehen
-
 <table style="margin: auto;">
     <tr>
         <td style="text-align: center;">
             <img src="https://cdn-icons-png.flaticon.com/128/9313/9313195.png" alt="Buchstabe A" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/a.png" alt="Zeichen A" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -17,7 +15,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/9326/9326434.png" alt="Buchstabe B" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/b.png" alt="Zeichen B" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -28,7 +26,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/3665/3665923.png" alt="Buchstabe C" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/c.png" alt="Zeichen C" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -39,7 +37,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/8142/8142823.png" alt="Buchstabe D" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/d.png" alt="Zeichen D" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -50,7 +48,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/11859/11859275.png" alt="Buchstabe E" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/e.png" alt="Zeichen E" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -61,7 +59,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/3665/3665934.png" alt="Buchstabe F" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/f.png" alt="Zeichen F" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -72,7 +70,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/9605/9605277.png" alt="Buchstabe G" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/g.png" alt="Zeichen G" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -83,7 +81,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/3665/3665946.png" alt="Buchstabe H" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/h.png" alt="Zeichen H" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -94,7 +92,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/7297/7297848.png" alt="Buchstabe I" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/i.png" alt="Zeichen I" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -105,7 +103,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/5379/5379270.png" alt="Buchstabe J" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/j.png" alt="Zeichen J" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -116,7 +114,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/5379/5379280.png" alt="Buchstabe K" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/k.png" alt="Zeichen K" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -127,7 +125,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/9605/9605282.png" alt="Buchstabe L" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/l.png" alt="Zeichen L" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -138,7 +136,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/8142/8142749.png" alt="Buchstabe M" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/m.png" alt="Zeichen M" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -149,7 +147,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/8142/8142756.png" alt="Buchstabe N" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/n.png" alt="Zeichen N" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -160,7 +158,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/7297/7297944.png" alt="Buchstabe O" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/o.png" alt="Zeichen O" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -171,7 +169,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/5540/5540757.png" alt="Buchstabe P" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/p.png" alt="Zeichen P" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -182,7 +180,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/3666/3666003.png" alt="Buchstabe Q" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/q.png" alt="Zeichen Q" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -193,7 +191,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/6819/6819272.png" alt="Buchstabe R" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/r.png" alt="Zeichen r" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -204,7 +202,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/8970/8970862.png" alt="Buchstabe S" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/s.png" alt="Zeichen S" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -215,7 +213,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/6819/6819288.png" alt="Buchstabe T" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/t.png" alt="Zeichen T" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -226,7 +224,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/9037/9037247.png" alt="Buchstabe U" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/u.png" alt="Zeichen U" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -237,7 +235,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/9313/9313248.png" alt="Buchstabe V" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/v.png" alt="Zeichen V" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -248,7 +246,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/8142/8142804.png" alt="Buchstabe W" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/w.png" alt="Zeichen W" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -259,7 +257,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/9037/9037253.png" alt="Buchstabe X" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/x.png" alt="Zeichen X" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -270,7 +268,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/3666/3666233.png" alt="Buchstabe Y" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img sc="/img/GebärdenBuchstaben/y.png" alt="Zeichen Y" width="200" height="200">
         </td>
     </tr>
 </table><br>
@@ -281,7 +279,7 @@ Hier wollen wir die Buchstaben sehen
             <img src="https://cdn-icons-png.flaticon.com/128/7298/7298100.png" alt="Buchstabe Z" width="200">
         </td>
         <td style="text-align: center;">
-            <img src="https://th.bing.com/th/id/OIP.FSvZDlMT0M8gfduff89T5QHaHk?w=196&h=200&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="Zeichen A" width="200">
+            <img src="/img/GebärdenBuchstaben/z.png" alt="Zeichen X" width="200" height="200">
         </td>
     </tr>
 </table><br>
