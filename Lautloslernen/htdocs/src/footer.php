0c344f38b60258ef71133e4f323025a7ccf3f7f3
sylvan013 <kai.keppler@web.de>
2024-03-21 11:13:48 +0100

    feat: reworked header & footer style

--- Lautloslernen/htdocs/src/footer.php
+++ Lautloslernen/htdocs/src/footer.php
@@ -1,3 +1,5 @@
 <footer>
-<a href="/?a=impressum">Impressum</a> | <a href="/?c=FAQ">FAQ</a> | Alle Inhalte ©LautlosLernen
+    <div class="nav">
+    <a href="/?a=impressum" class="nav-link">Impressum</a> &copy;LautlosLernen <a href="/?c=FAQ" class="nav-link">FAQ</a>
+    </div>
 </footer>
