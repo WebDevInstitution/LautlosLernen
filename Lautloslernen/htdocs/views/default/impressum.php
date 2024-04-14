af9ea5be1d8b85d86a62a11bd8c410875a251e23
sylvan013 <kai.keppler@web.de>
2024-04-14 18:15:57 +0200

    feat: correct impressum

--- Lautloslernen/htdocs/views/default/impressum.php
+++ Lautloslernen/htdocs/views/default/impressum.php
@@ -1,13 +1,13 @@
 <h1>Impressum und Haftungsausschluss</h1>
 
 <h2>Verantwortlich für den Inhalt:</h2>
-<p>Lautloslernen<br>
+<p>LautlosLernen<br>
 Musterstraße 1a<br>
 Stuttgart 70174<br>
-lautlos@lernen.com<br></p>
+kontakt@lautoslernen.com<br></p>
 
 <h2>Haftungsausschluss:</h2>
-<p>Die Informationen auf diesem Blog wurden sorgfältig recherchiert und zusammengestellt. Dennoch kann keine Gewähr für die Aktualität, Richtigkeit und Vollständigkeit der bereitgestellten Inhalte übernommen werden. Jegliche Haftung für Schäden, die direkt oder indirekt aus der Nutzung dieser Website entstehen, wird ausgeschlossen.</p>
+<p>Die Informationen auf dieser Website wurden sorgfältig recherchiert und zusammengestellt. Dennoch kann keine Gewähr für die Aktualität, Richtigkeit und Vollständigkeit der bereitgestellten Inhalte übernommen werden. Jegliche Haftung für Schäden, die direkt oder indirekt aus der Nutzung dieser Website entstehen, wird ausgeschlossen.</p>
 
 <h2>Urheberrecht:</h2>
 <p>Die auf dieser Website veröffentlichten Inhalte und Werke unterliegen dem deutschen Urheberrecht. Jede Art der Verwertung, Vervielfältigung, Bearbeitung oder Verbreitung außerhalb der Grenzen des Urheberrechts bedarf der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.</p>
@@ -16,7 +16,7 @@ lautlos@lernen.com<br></p>
 <p>Diese Website enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p>
 
 <h2>Datenschutzerklärung:</h2>
-<p>Unsere Datenschutzerklärung findest du unter dem folgenden Link: <a href="https://theuselessweb.site/boohbah-zone/">Datenschutzerklärung</a></p>
+<p>Unsere Datenschutzerklärung findest du unter dem folgenden Link: <a href="#">Datenschutzerklärung</a></p>
 
 <h2>Bildnachweise:</h2>
 <p>Die auf dieser Website verwendeten Bilder stammen entweder aus eigenen Quellen, sind lizenziert oder unterliegen dem Creative Commons Lizenzmodell. Genauere Informationen zu den Bildnachweisen sind auf Anfrage erhältlich.</p>
